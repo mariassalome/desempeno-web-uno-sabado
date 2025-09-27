078c09ce4b8a1b9a7c55c24cd5ac9b533cf29abd
mariassalome <salomealzate888@gmail.com>
2025-09-27 11:50:11 -0500

    Fase 2: Implementadas funciones para alimentar y jugar con la mascota

--- 1033186196-MARIA-SALOME-ALZATE-PAMPLONA/app.js
+++ 1033186196-MARIA-SALOME-ALZATE-PAMPLONA/app.js
@@ -7,3 +7,28 @@ function mostrarEstado() {
   console.log(`Mascota: ${nombreMascota}, Hambre: ${nivelHambre}/100, Felicidad: ${nivelFelicidad}/100.`);
 }
 mostrarEstado();
+
+
+const alimentar = function(puntosComida) {
+  nivelHambre -= puntosComida;
+
+  if (nivelHambre < 0) {
+    nivelHambre = 0;
+  }
+  console.log(`Ñam! El hambre de ${nombreMascota} ha disminuido. Hambre: ${nivelHambre}/100.`);
+};
+
+
+const jugar = (puntosDiversion) => {
+  nivelFelicidad += puntosDiversion;
+  
+  if (nivelFelicidad > 100) {
+    nivelFelicidad = 100;
+  }
+  console.log(`Que divertido! La felicidad de ${nombreMascota} ha aumentado. Felicidad: ${nivelFelicidad}/100.`);
+};
+ 
+alimentar(5); 
+jugar(40);
+
+mostrarEstado()
