b37079df6a5f8977db069eb20e962ac1a841acba
mariassalome <salomealzate888@gmail.com>
2025-09-27 12:02:19 -0500

    Fase 3: Añadida simulación de tiempo y diagnóstico de la mascota

--- 1033186196-MARIA-SALOME-ALZATE-PAMPLONA/app.js
+++ 1033186196-MARIA-SALOME-ALZATE-PAMPLONA/app.js
@@ -32,3 +32,49 @@ alimentar(5);
 jugar(40);
 
 mostrarEstado()
+
+
+const simularPasoDelTiempo = () => {
+  for (let i = 0; i < 4; i++) { 
+    nivelHambre += 10;
+    nivelFelicidad -= 5;
+
+    
+    if (nivelHambre > 100) {
+      nivelHambre = 100;
+    }
+    if (nivelFelicidad < 0) {
+      nivelFelicidad = 0;
+    }
+  }
+  console.log("Han pasado algunas horas...");
+};
+
+
+function diagnosticoMascota() {
+  
+  if (nivelHambre >= 80) {
+    console.log("¡Alertaaa! Tu mascota está muy hambrienta, necesita comida ya!");
+  } else if (nivelHambre >= 40) {
+    console.log("Tu mascota tiene algo de hambre.");
+  } else {
+    console.log("Tu mascota está satisfecha.");
+  }
+
+ 
+  switch (true) {
+    case (nivelFelicidad >= 80):
+      console.log("Tu mascota está muy feliz y juguetona :D");
+      break;
+    case (nivelFelicidad >= 40):
+      console.log("Tu mascota está contenta :)");
+      break;
+    default:
+      console.log("Tu mascota se siente un poco triste T-T");
+      break;
+  }
+}
+
+simularPasoDelTiempo(15);
+diagnosticoMascota(5);
+
